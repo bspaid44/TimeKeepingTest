943781b6beebe5b6777dd78354918c3608401cb4
bspaid44 <brandonspaid@gmail.com>
2024-04-12 20:58:59 -0400

    added client crud operations

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -19,5 +19,51 @@ namespace TimeTest.Controllers
             ClientIndexViewModel clientIndexViewModel = new ClientIndexViewModel(_clientRepository.Clients);
             return View(clientIndexViewModel);
         }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Client client)
+        {
+            if (TryValidateModel(client))
+            {
+                _clientRepository.SaveClient(client);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(client);
+            }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Client client = _clientRepository.Clients.FirstOrDefault(c => c.Id == id);
+            return View(client);
+        }
+
+        public IActionResult Update(Client client)
+        {
+            if (TryValidateModel(client))
+            {
+                _clientRepository.UpdateClient(client);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(client);
+            }
+        }
+
+        public RedirectToActionResult Delete(int id)
+        {
+            Client client = _clientRepository.Clients.FirstOrDefault(c => c.Id == id);
+            _clientRepository.DeleteClient(client);
+            return RedirectToAction("Index");
+        }
     }
 }
